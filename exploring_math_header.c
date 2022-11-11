0e782031b5a2e14e16efb8641e0cdbf6764b22c3
syedalihasany <49318734+syedalihasany@users.noreply.github.com>
2022-11-11 16:35:30 +0500

    added some new lines to the source code

--- exploring_math_header.c
+++ exploring_math_header.c
@@ -7,7 +7,14 @@
     #define PI 3.14159265358979323846
 #endif
 
-int main() {
+/*
+ * use the following command in the command line to compile using C99
+ * 
+ * >> gcc -std=c99 -o outputfile sourcefile.c
+ * 
+ */
+
+int main(int argc,char* argv[]) {
     // Write C code here
     printf("%d \n",FLT_ROUNDS);
     printf("%d \n",FLT_MIN_10_EXP);
@@ -16,7 +23,14 @@ int main() {
     printf("%d \n",DECIMAL_DIG);
     printf("%d \n",INT_MAX);
     printf("%f \n",cos(PI/4));
+    printf("%f \n",exp(3));
+    printf("%f \n",log(20.085537));
     
+    
+    //printing command line arguments
+    for (int i = 0; i < argc ; i++){
+        printf("argument %d is %s \n",i,argv[i]);
+    }
     
 
     return 0;
