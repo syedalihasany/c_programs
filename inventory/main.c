66510a026632a7018b3defa7cfedab86becc9206
syedalihasany <49318734+syedalihasany@users.noreply.github.com>
2022-12-08 20:03:39 +0500

    Inverntory program for structures

---
+++ inventory/main.c
@@ -0,0 +1,18 @@
+#include "myfunctions.h"
+#include "myfunctions.c"
+#define MAX 100
+
+struct part {
+    *char name;
+    int number;
+    int quantity;
+};
+
+int main(){
+    struct part inventory[MAX];
+
+
+
+
+    return 0;
+}
---
+++ inventory/myfunctions.h
@@ -0,0 +1,13 @@
+#ifndef MYFUCTIONS_H
+#define MYFUCTIONS_H
+
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+
+
+
+#endif
